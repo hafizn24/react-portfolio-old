c805630261a61dc35bcd75affe49782874322718
hafiz <hafizn24@gmail.com>
2022-08-12 12:44:49 +0800

    created api system

--- src/components/API_1.js
+++ src/components/API_1.js
@@ -1,7 +1,32 @@
+import React, {useEffect, useState} from "react"
+
 function API_1() {
+  const [character, setCharacter] = useState("")
+  const [image, setImage] = useState("")
+  
+  useEffect(() => {
+    const url = 'https://rickandmortyapi.com/api/character'
+
+    const fetchData = async() =>{
+      try{
+        const response = await fetch(url)
+        const json = await response.json()
+        setCharacter(json.results[0].name)
+        setImage(json.results[0].image)
+        console.log(json)
+      }
+      catch(error){
+        console.log('error')
+      }
+    }
+
+    fetchData()
+  },[])
+
   return (
     <div>
-        <h1>API Page</h1>
+        {character}
+        <img src={image} alt="Image" />
     </div>
   )
 }
